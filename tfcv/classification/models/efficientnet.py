31688cb55a10bfe0ab7db2f6e38b096ebc37cba4
lyr <yirenlin1125@gmail.com>
2022-02-07 15:54:03 +0800

    add eff

---
+++ tfcv/classification/models/efficientnet.py
@@ -0,0 +1,203 @@
+import tensorflow as tf
+import re
+import numpy as np
+
+from tfcv.config import AttrDict
+
+EFFICIENTNET_PARAMS = dict()
+
+for k, v in {
+        # (width_coefficient, depth_coefficient, resolution, dropout_rate)
+        'efficientnet-b0': (1.0, 1.0, 224, 0.2),
+        'efficientnet-b1': (1.0, 1.1, 240, 0.2),
+        'efficientnet-b2': (1.1, 1.2, 260, 0.3),
+        'efficientnet-b3': (1.2, 1.4, 300, 0.3),
+        'efficientnet-b4': (1.4, 1.8, 380, 0.4),
+        'efficientnet-b5': (1.6, 2.2, 456, 0.4),
+        'efficientnet-b6': (1.8, 2.6, 528, 0.5),
+        'efficientnet-b7': (2.0, 3.1, 600, 0.5),
+        'efficientnet-b8': (2.2, 3.6, 672, 0.5),
+        'efficientnet-l2': (4.3, 5.3, 800, 0.5),
+    }.items():
+    EFFICIENTNET_PARAMS[k] = AttrDict()
+    EFFICIENTNET_PARAMS[k].from_dict(
+        dict(
+            width_coefficient = v[0],
+            depth_coefficient = v[1],
+            resolution = v[2],
+            dropout_rate = v[3])
+        )
+
+_DEFAULT_BLOCKS_ARGS = [
+    'r1_k3_s11_e1_i32_o16_se0.25', 'r2_k3_s22_e6_i16_o24_se0.25',
+    'r2_k5_s22_e6_i24_o40_se0.25', 'r3_k3_s22_e6_i40_o80_se0.25',
+    'r3_k5_s11_e6_i80_o112_se0.25', 'r4_k5_s22_e6_i112_o192_se0.25',
+    'r1_k3_s11_e6_i192_o320_se0.25',
+]
+
+class BlockDecoder(object):
+    """Block Decoder for readability."""
+
+    def _decode_block_string(self, block_string):
+        """Gets a block through a string notation of arguments."""
+        assert isinstance(block_string, str)
+        ops = block_string.split('_')
+        options = {}
+        for op in ops:
+            splits = re.split(r'(\d.*)', op)
+            if len(splits) >= 2:
+                key, value = splits[:2]
+                options[key] = value
+
+        if 's' not in options or len(options['s']) != 2:
+            raise ValueError('Strides options should be a pair of integers.')
+
+        param_dict =dict(
+            kernel_size=int(options['k']),
+            num_repeat=int(options['r']),
+            input_filters=int(options['i']),
+            output_filters=int(options['o']),
+            expand_ratio=int(options['e']),
+            id_skip=('noskip' not in block_string),
+            se_ratio=float(options['se']) if 'se' in options else None,
+            strides=[int(options['s'][0]),
+                    int(options['s'][1])],
+            conv_type=int(options['c']) if 'c' in options else 0,
+            fused_conv=int(options['f']) if 'f' in options else 0,
+            super_pixel=int(options['p']) if 'p' in options else 0,
+            condconv=('cc' in block_string))
+
+    def _encode_block_string(self, block):
+        """Encodes a block to a string."""
+        args = [
+            'r%d' % block.num_repeat,
+            'k%d' % block.kernel_size,
+            's%d%d' % (block.strides[0], block.strides[1]),
+            'e%s' % block.expand_ratio,
+            'i%d' % block.input_filters,
+            'o%d' % block.output_filters,
+            'c%d' % block.conv_type,
+            'f%d' % block.fused_conv,
+            'p%d' % block.super_pixel,
+        ]
+        if block.se_ratio > 0 and block.se_ratio <= 1:
+            args.append('se%s' % block.se_ratio)
+        if block.id_skip is False:  # pylint: disable=g-bool-id-comparison
+            args.append('noskip')
+        if block.condconv:
+            args.append('cc')
+        return '_'.join(args)
+
+    def decode(self, string_list):
+        """Decodes a list of string notations to specify blocks inside the network.
+
+        Args:
+            string_list: a list of strings, each string is a notation of block.
+
+        Returns:
+            A list of namedtuples to represent blocks arguments.
+        """
+        assert isinstance(string_list, list)
+        blocks_args = []
+        for block_string in string_list:
+            blocks_args.append(self._decode_block_string(block_string))
+        return blocks_args
+
+    def encode(self, blocks_args):
+        """Encodes a list of Blocks to a list of strings.
+
+        Args:
+            blocks_args: A list of namedtuples to represent blocks arguments.
+        Returns:
+            a list of strings, each string is a notation of block.
+        """
+        block_strings = []
+        for block in blocks_args:
+            block_strings.append(self._encode_block_string(block))
+        return block_strings
+
+def round_filters(
+    width_coefficient, 
+    depth_divisor, 
+    min_depth, 
+    filters, 
+    skip=False):
+    """Round number of filters based on depth multiplier."""
+    multiplier = width_coefficient
+    divisor = depth_divisor
+    min_depth = min_depth
+    if skip or not multiplier:
+        return filters
+
+    filters *= multiplier
+    min_depth = min_depth or divisor
+    new_filters = max(min_depth, int(filters + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_filters < 0.9 * filters:
+        new_filters += divisor
+    return int(new_filters)
+
+def conv_kernel_initializer(shape, dtype=None, partition_info=None):
+    """Initialization for convolutional kernels.
+
+    The main difference with tf.variance_scaling_initializer is that
+    tf.variance_scaling_initializer uses a truncated normal with an uncorrected
+    standard deviation, whereas here we use a normal distribution. Similarly,
+    tf.initializers.variance_scaling uses a truncated normal with
+    a corrected standard deviation.
+
+    Args:
+        shape: shape of variable
+        dtype: dtype of variable
+        partition_info: unused
+
+    Returns:
+        an initialization for the variable
+    """
+    del partition_info
+    kernel_height, kernel_width, _, out_filters = shape
+    fan_out = int(kernel_height * kernel_width * out_filters)
+    return tf.random.normal(
+        shape, mean=0.0, stddev=np.sqrt(2.0 / fan_out), dtype=dtype)
+        
+class Stem(tf.keras.layers.Layer):
+    """Stem layer at the begining of the network."""
+
+    def __init__(
+        self, 
+        global_params, 
+        norm_momentum=0.99,
+        norm_epsilon=1e-3,
+        data_format='channels_last', 
+        name=None):
+        super(Stem, self).__init__(name=name)
+        self._conv_stem = tf.keras.layers.Conv2D(
+            filters=round_filters(stem_filters, global_params,
+                                global_params.fix_head_stem),
+            kernel_size=[3, 3],
+            strides=[2, 2],
+            kernel_initializer=conv_kernel_initializer,
+            padding='same',
+            data_format=global_params.data_format,
+            use_bias=False)
+        self._bn = global_params.batch_norm(
+            axis=(1 if data_format == 'channels_first' else -1),
+            momentum=norm_momentum,
+            epsilon=norm_epsilon)
+        self._relu_fn = global_params.relu_fn or tf.nn.swish
+
+    def call(self, inputs, training):
+        return self._relu_fn(self._bn(self._conv_stem(inputs), training=training))
+
+class EfficientNet(tf.keras.Model):
+    def __init__(
+        self,
+        name,
+        activation='swish',
+        ):
+        assert name in EFFICIENTNET_PARAMS, 'unknown model name'
+        config = EFFICIENTNET_PARAMS[name]
+
+        stem = Stem(config, self._blocks_args[0].input_filters)
+
+    pass
